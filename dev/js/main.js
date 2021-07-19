6aa732c8d0b8b4800dc145010604daff7fd3ef4d
Pim <vilhjalmar.kennedy@gmail.com>
2021-07-19 18:08:31 +0200

    f

--- dev/js/main.js
+++ dev/js/main.js
@@ -425,8 +425,8 @@ function delete_cookie(name) {
     document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.', window.location.host.toString()].join('');
 }
 
-function see(){
-    renderList(element.value);
+function see(delay){
+    renderList(delay);
     updateInformation();
 }
 
