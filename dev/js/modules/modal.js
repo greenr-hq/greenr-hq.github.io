de4f3b235990d876288ad203aecfd15c52959d98
Pim <vilhjalmar.kennedy@gmail.com>
2021-07-19 19:56:12 +0200

    f

--- dev/js/modules/modal.js
+++ dev/js/modules/modal.js
@@ -1,17 +1,25 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// select the open-btn button
+let openBtn = document.getElementById('open-btn');
+// select the modal-background
+let modalBackground = document.getElementById('modal-background');
+// select the close-btn 
+let closeBtn = document.getElementById('close-btn');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// shows the modal when the user clicks open-btn
+openBtn.addEventListener('click', function() {
+  modalBackground.style.display = 'block';
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// hides the modal when the user clicks close-btn
+closeBtn.addEventListener('click', function() {
+  modalBackground.style.display = 'none';
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+// hides the modal when the user clicks outside the modal
+window.addEventListener('click', function(event) {
+  // check if the event happened on the modal-background
+  if (event.target === modalBackground) {
+    // hides the modal
+    modalBackground.style.display = 'none';
   }
-}
+});
